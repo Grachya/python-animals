c8f79d6728d81025afd7edb409117048e0a572cd
Grachya <grach41@gmail.com>
2019-01-25 14:42:47 +0300

    Get animals total height and print the heaviest animal.

--- animals.py
+++ animals.py
@@ -1,9 +1,17 @@
+# animal classes
 class Animal:
-    def __init__(self, name, voice_type):
+    feed_state = 'hungry'
+
+    def __init__(self, name, voice_type, weight):
         self.name = name
         self.voice_type = voice_type
+        self.weight = weight
 
-    feed_state = 'hungry'
+    def __add__(self, other):
+        return self.weight + other.weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
 
     def feed(self):
         self.feed_state = 'full'
@@ -12,45 +20,66 @@ class Animal:
     def voice(self):
         print(self.voice_type)
 
+
 class Goose(Animal):
+    eggs_state = 'full'
+
     def collect_eggs(self):
+        self.eggs_state = 'empty'
         print('Eggs were collected from {} goose'.format(self.name))
 
 
 class Cow(Animal):
+    milk_state = 'full'
+
     def milking(self):
+        self.milk_state = 'empty'
         print('Cow {} was milked'.format(self.name))
 
 
 class Sheep(Animal):
+    wool_state = 'full'
+
     def shear(self):
+        self.wool_state = 'empty'
         print('Sheep {} was sheared'.format(self.name))
 
 
 class Chicken(Animal):
+    eggs_state = 'full'
+
     def collect_eggs(self):
+        self.eggs_state = 'empty'
         print('Eggs were collected from {} chicken'.format(self.name))
 
 
 class Goat(Animal):
+    milk_state = 'full'
+
     def milking(self):
+        self.milk_state = 'empty'
         print('Goat {} was milked'.format(self.name))
 
 
 class Duck(Animal):
+    eggs_state = 'full'
+
     def collect_eggs(self):
+        self.eggs_state = 'empty'
         print('Eggs were collected from {} duck'.format(self.name))
 
+# animal instances
 
 
-grey_goose = Goose('Серый', 'га-га-га')
-white_goose = Goose('Белый', 'га-га-га')
-cow = Cow('Манька', 'мууу')
-rammy_sheep = Sheep('Барашек', 'бееее')
-cyrly_sheep = Sheep('Кудрявый', 'бееее')
-goat_horns = Goat('Рога', 'мееее')
-goat_hoofs = Goat('Копыта', 'мееее')
-mallard_duck = Duck('Кряква', 'кря-кря')
+grey_goose = Goose('Серый', 'га-га-га', 12)
+white_goose = Goose('Белый', 'га-га-га', 14)
+cow = Cow('Манька', 'мууу', 104)
+rammy_sheep = Sheep('Барашек', 'бееее', 52)
+cyrly_sheep = Sheep('Кудрявый', 'бееее', 56)
+goat_horns = Goat('Рога', 'мееее', 47)
+goat_hoofs = Goat('Копыта', 'мееее', 43)
+mallard_duck = Duck('Кряква', 'кря-кря', 16)
+
 
 # feed all animals
 
@@ -59,17 +88,43 @@ all_animals = [grey_goose, white_goose, cow, rammy_sheep, cyrly_sheep, goat_horn
 for animal in all_animals:
     print(animal.feed())
 
+# some action with each animal
+
+grey_goose.collect_eggs()
+white_goose.collect_eggs()
+cow.milking()
+rammy_sheep.shear()
+cyrly_sheep.shear()
+goat_horns.milking()
+goat_hoofs.milking()
+mallard_duck.collect_eggs()
+
+# total animals weight
 
 
+def get_total_animals_weight():
+
+    total_weight = 0
+
+    for animal in all_animals:
+        total_weight += animal.weight
+
+    return total_weight
 
 
+print('Total animals weight is {}'.format(get_total_animals_weight()))
+
+# the heaviest animal
 
 
+def get_heaviest_animal_name():
+    heaviest_animal = all_animals[0]
+
+    for animal_inst in all_animals:
+        if heaviest_animal < animal_inst:
+            heaviest_animal = animal_inst
+
+    return heaviest_animal.name
 
 
-
-
-
-
-
-
+print('The heaviest animal is {}'.format(get_heaviest_animal_name()))
