e68650b9d124bb8899634c4b3c495d3582e45134
Grachya <Grach41@gmail.com>
2019-02-12 21:36:16 +0300

    Add new functionality

--- requests/example.py
+++ requests/example.py
@@ -1,9 +1,11 @@
 import requests
+import codecs
 
 API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(text, to_lang):
+
+def translate_it(file_path, result_path, from_lang, to_lang='ru'):
     """
     https://translate.yandex.net/api/v1.5/tr.json/translate ?
     key=<API-ключ>
@@ -13,24 +15,32 @@ def translate_it(text, to_lang):
      & [options=<опции перевода>]
      & [callback=<имя callback-функции>]
 
+    :param file_path:
+    :param from_lang:
     :param to_lang:
+    :param result_path:
     :return:
     """
 
+    with open(file_path, 'r') as f:
+        data = f.read().strip()
+
     params = {
         'key': API_KEY,
-        'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'text': data,
+        'lang': '{}-{}'.format(from_lang, to_lang),
     }
 
     response = requests.get(URL, params=params)
     json_ = response.json()
-    return ''.join(json_['text'])
+
+    with codecs.open(f'translated-to-{to_lang}-{result_path}', 'w', 'utf-8') as file:
+        file.write(''.join(json_['text']))
 
 
-# print(translate_it('В настоящее время доступна единственная опция —
-# признак включения в ответ автоматически определенного языка переводимого текста.
-# Этому соответствует значение 1 этого параметра.', 'no'))
+files = ['DE.txt', 'ES.txt', 'FR.txt']
 
-
-requests.post('http://requestb.in/10vc0zh1', json=dict(a='goo', b='foo'))
+for item in files:
+    file_lang = item.split('.')[0].lower()
+    result = item
+    translate_it(file_path=item, from_lang=file_lang, result_path=result)
