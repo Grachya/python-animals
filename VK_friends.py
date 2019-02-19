dbec52b70af7c9bdf3700522e474987457433380
Grachya <Grach41@gmail.com>
2019-02-19 18:06:27 +0300

    Refactor by comments
    -add str overload
    -add and overload
    -get list of user instances

--- VK_friends.py
+++ VK_friends.py
@@ -1,15 +1,40 @@
 import requests
+import time
 
-TOKEN = 'e9dcdb57d7f7eb72a0a962bed4549466b01c5e581875f95c037b4246baaca5289969fce94a724a1f286b5'
+TOKEN = 'bd878a0562f9e0a7b89fbbc575c60f7a9dc67df73c5b7ab25cb556a9aa71f8ecc0c069671657694e88d7f'
 
 class User:
-    def __init__(self, token):
-        self.token = token
+    def __init__(self, user_id=None):
+        self.user_id = user_id
 
-    def get_friends(self, user_id=''):
+    def __str__(self):
+        return self.get_profile_url()
+
+    def __and__(self, other):
+        left_operand_friends = self.get_friends(self.user_id)
+        right_operand_friends = self.get_friends(other.user_id)
+
+        return self.get_intersection(left_operand_friends, right_operand_friends)
+
+    def get_profile_url(self):
         params = {
             'v': '5.92',
-            'access_token': self.token,
+            'access_token': TOKEN,
+            'user_id': self.user_id,
+            'fields': 'screen_name'
+        }
+        res = requests.get(
+            'https://api.vk.com/method/users.get',
+            params)
+
+        screen_name = res.json()['response'][0]['screen_name']
+        profile_url = f'https://vk.com/{screen_name}'
+        return profile_url
+
+    def get_friends(self, user_id=None):
+        params = {
+            'v': '5.92',
+            'access_token': TOKEN,
         }
         if user_id:
             params['user_id'] = user_id
@@ -18,33 +43,28 @@ class User:
             params)
         return res.json()['response']['items']
 
-    def get_users(self, user_ids):
-        params = {
-            'v': '5.92',
-            'access_token': self.token,
-            'user_ids': user_ids,
-            'fields': 'true',
-        }
-        res = requests.get(
-            'https://api.vk.com/method/users.get',
-            params)
-        return res.json()
-
     def get_intersection(self, list_1, list_2):
-        inters = list(set(list_1) & set(list_2))
-        for k, item in enumerate(inters):
-            inters[k] = str(item)
-        return ','.join(inters)
+        common_friends = list(set(list_1) & set(list_2))
+
+        intersection_friends = []
+        for friend in common_friends:
+            user = User(friend)
+            intersection_friends.append(user)
+        return intersection_friends
 
 
-user = User(TOKEN)
-own_friends = user.get_friends()
-x_friend = own_friends[0]
-x_friend_name = user.get_users(x_friend)
-x_friend_friends = user.get_friends(x_friend)
-intersection = user.get_intersection(own_friends, x_friend_friends)
-intersection_users = user.get_users(intersection)['response']
+some_friend_id = '143058'
 
-print(f'Наши общие друзья с {x_friend_name["response"][0]["first_name"]}:')
-for user in intersection_users:
-    print(user['first_name'], user['last_name'])
+user_1 = User()
+user_2 = User(some_friend_id)
+
+print(f'Ссылка на профиль пользователя 1: {user_1}')
+print(f'Ссылка на профиль пользователя 2: {user_2}')
+
+common = user_1 & user_2
+
+for item in common:
+    print(f'Профиль обещего друга: {item}')
+    time.sleep(1)
+
+
